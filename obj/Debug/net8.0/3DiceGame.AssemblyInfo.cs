c22467597a434c1c0cfce7296b57e9016908a028
SanjithManne <109084240+SanjithManne@users.noreply.github.com>
2024-02-22 12:31:20 -0600

    Update 1

--- obj/Debug/net8.0/3DiceGame.AssemblyInfo.cs
+++ obj/Debug/net8.0/3DiceGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("3DiceGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f4f7e697e133be116da00134b1708bb75193af3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3725ea70aa3d55a9c59469e1b97002e119997561")]
 [assembly: System.Reflection.AssemblyProductAttribute("3DiceGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("3DiceGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
